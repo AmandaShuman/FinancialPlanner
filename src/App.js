aceefe492d62b3d941c51cd5786c8f8de49cdf45
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-03-30 22:14:11 -0400

    adding expenses in

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AddExpenseForm from './components/AddExpenseForm';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar color="success">
-          <div className="container">
-            <NavbarBrand href="/">Financial Planning</NavbarBrand>
-          </div>
-        </Navbar>
+const App = () => {
+  return (
+    <div className='container'>
+      <h1 className='mt-3'>My Budget Planner</h1>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <Budget />
+        </div>
+        <div className='col-sm'>
+          <Remaining />
+        </div>
+        <div className='col-sm'>
+          <ExpenseTotal />
+        </div>
       </div>
-    );
-  }
-}
+      <h3 className='mt-3'>Expenses</h3>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <ExpenseList />
+        </div>
+      </div>
+      <h3 className='mt-3'>Add Expense</h3>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <AddExpenseForm />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
